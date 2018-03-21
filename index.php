742ad4ae5e204be34da75408093c0085c3bc5f4b
Jaime Andres <jamoreno528@gmail.com>
2018-03-21 11:46:39 -0500

    Inicio prueba Repositorio

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!--<META HTTP-EQUIV="Refresh" CONTENT="3;URL=inicio.php">-->
+    <title>Intro >> JM Fotografía petss</title>
+    <link rel="stylesheet" href="css/estilos.css">
+  </head>
+  <body>
+      <img class="fondoImg" src="img/fondo.jpg"/>
+    <div class="logoDiv">
+      <img class="logoImg" src="img/logo.png"/>
+    </div>
+    <div class="fdDiv" >
+      <a href="inicio.php"><img class="fdImg" src="img/fd.png"/></a>
+    </div>
+  </body>
+</html>
